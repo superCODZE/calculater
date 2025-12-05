36bb9e12bfbe9b033cf1d72e366c7e5105a20a84
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2025-12-05 12:29:17 +0100

    add DisplayResult and ClearAll fonctions

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let result = document.querySelector('#display');
 function EnterValue(arg){
     result.value += arg;
 }
+
+function ClearAll(){
+    result.value = '';
+}
+
+function DisplayResult(){
+    result.value = eval(result.value);
+}
