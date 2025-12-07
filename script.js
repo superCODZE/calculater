20406ec19c7983467234a66e95988193af093080
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2025-12-07 21:03:11 +0100

    add error display

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ function ClearAll(){
 }
 
 function DisplayResult(){
-    result.value = eval(result.value);
+    try{
+        result.value = eval(result.value);
+        
+    }
+    catch(err){
+        result.value = 'Error';
+    }
+    
 }
 
 function DeleteLastCharacter(){
     result.value = result.value.slice(0, result.value.length - 1);
 }
+
